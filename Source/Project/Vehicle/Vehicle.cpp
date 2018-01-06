be42a66a6be583bc017d3ae41a92e5d972e52594
arcashka <pewpewstol@gmail.com>
2018-01-06 22:46:05 +0700

    Initial Commit

---
+++ Source/Project/Vehicle/Vehicle.cpp
@@ -0,0 +1,59 @@
+#include "Vehicle.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+
+AVehicle::AVehicle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpringArm"));
+	CameraSpringArm->SetupAttachment(CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent")));
+	CameraSpringArm->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 50.0f), FRotator(-60.0f, 0.0f, 0.0f));
+	CameraSpringArm->TargetArmLength = 400.f;
+	CameraSpringArm->bEnableCameraLag = true;
+	CameraSpringArm->CameraLagSpeed = 3.0f;
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("GameCamera"));
+	Camera->SetupAttachment(CameraSpringArm, USpringArmComponent::SocketName);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+void AVehicle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!MovementInput.IsZero())
+	{
+		MovementInput = MovementInput.GetSafeNormal() * 100.0f;
+		FVector NewLocation = GetActorLocation();
+		NewLocation += GetActorForwardVector() * MovementInput.X * DeltaTime;
+		NewLocation += GetActorRightVector() * MovementInput.Y * DeltaTime;
+		SetActorLocation(NewLocation);
+	}
+}
+
+void AVehicle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AVehicle::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AVehicle::MoveRight);
+}
+
+void AVehicle::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AVehicle::MoveForward(float AxisValue)
+{
+	MovementInput.X = FMath::Clamp<float>(AxisValue, -1.0f, 1.0f);
+}
+
+void AVehicle::MoveRight(float AxisValue)
+{
+	MovementInput.Y = FMath::Clamp<float>(AxisValue, -1.0f, 1.0f);
+}
+
---
+++ Source/Project/Vehicle/Vehicle.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/Pawn.h"
+
+#include "Vehicle.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class PROJECT_API AVehicle : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	AVehicle();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+
+	USpringArmComponent* CameraSpringArm;
+	UCameraComponent* Camera;
+
+	FVector2D MovementInput;
+};
